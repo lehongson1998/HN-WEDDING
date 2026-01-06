8d2e617850f6039276cd1b73f5ecbb6f50360b4d
lehongson1998 <104547574+lehongson1998@users.noreply.github.com>
2026-01-06 12:50:54 +0700

    Remove loadGuestName function
    Removed loadGuestName function and its invocation.

--- script.js
+++ script.js
@@ -108,21 +108,6 @@ function initSnow() {
   draw();
 }
 
-function loadGuestName() {
-  const params = new URLSearchParams(window.location.search);
-  const name = params.get("to");
-
-  const guestEl = document.getElementById("guestName");
-
-  if (name) {
-    guestEl.textContent = decodeURIComponent(name);
-  } else {
-    guestEl.textContent = "Bạn";
-  }
-}
-
-loadGuestName();
-
 /* ===============================
    SAKURA
 ================================ */
